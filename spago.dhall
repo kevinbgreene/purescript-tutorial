7dd9367608cd6830e2d1c38d66bd89954fd49a63
Kevin Greene <hightimesteddy@gmail.com>
2020-04-26 18:14:22 -0700

    input event handling

--- spago.dhall
+++ spago.dhall
@@ -4,7 +4,14 @@ You can edit this file as you like.
 -}
 { name = "my-project"
 , dependencies =
-  [ "arrays", "console", "effect", "psci-support", "web-dom", "web-html" ]
+  [ "arrays"
+  , "console"
+  , "effect"
+  , "psci-support"
+  , "web-dom"
+  , "web-events"
+  , "web-html"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -4,30 +4,52 @@ import Prelude
 
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Console (log)
 
 import Web.HTML (window)
 import Web.HTML.Window (document)
-import Web.HTML.HTMLDocument (toParentNode, HTMLDocument)
+import Web.HTML.HTMLDocument (toEventTarget, toParentNode, HTMLDocument)
+import Web.HTML.HTMLInputElement (fromEventTarget, value, HTMLInputElement)
 
 import Web.DOM.Element (Element, toNode)
-import Web.DOM.Node (textContent)
+import Web.DOM.Node (setTextContent)
 import Web.DOM.ParentNode (querySelector, QuerySelector(QuerySelector))
+
+import Web.Event.Event (Event, EventType(..), target)
+import Web.Event.EventTarget (EventTarget, eventListener, addEventListener)
 
 rootSelector :: QuerySelector
 rootSelector = QuerySelector ("#root")
 
-maybeText :: Maybe Element -> Effect String
-maybeText (Just el) = textContent  (toNode el)
-maybeText _ = pure ""
+inputEvent :: EventType
+inputEvent = EventType ("input")
 
 selectFromDocument :: HTMLDocument -> Effect (Maybe Element)
 selectFromDocument doc = querySelector rootSelector (toParentNode doc)
+
+updateText :: String -> Maybe Element -> Effect Unit
+updateText str (Just el) = setTextContent str (toNode el)
+updateText _ _ = pure unit
+
+inputValue :: Maybe HTMLInputElement -> Effect String
+inputValue (Just el) = value el
+inputValue _ = pure ""
+
+targetValue :: Maybe EventTarget -> Effect String
+targetValue (Just et) = inputValue (fromEventTarget et)
+targetValue _ = pure ""
+
+eventValue :: Event -> Effect String
+eventValue evt = targetValue (target evt)
+
+inputEventHandler :: Maybe Element -> Event -> Effect Unit
+inputEventHandler el evt = do 
+  str <- eventValue evt
+  updateText str el
 
 main :: Effect Unit
 main = do
   w <- window
   d <- document w
   el <- selectFromDocument d
-  str <- maybeText el
-  log str
+  eh <- eventListener (inputEventHandler el)
+  addEventListener inputEvent eh true (toEventTarget d)
